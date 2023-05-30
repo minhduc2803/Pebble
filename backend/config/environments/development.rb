2bccf4d97aff7715bccaf6540e46914859c62dc1
minhduc2803 <minhduc2803@gmail.com>
2023-05-30 22:19:02 +0700

    allow hosts

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.server_timing = true
 
   # Allow requests from the host defined in SERVER_HOST env variable
-  config.hosts << ENV.fetch('SERVER_HOST', nil)
+  config.hosts << /[a-z0-9]+\.c9users\.io/
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
